cf9f203659b01681a015b65743dbe9cc55dfebf7
RINEARN <commit@rinearn.com>
2025-12-01 20:33:34 +0900

    Add: new variable: START, END, CONTAIN, ..._PATTERN, FULL_PATTERN
    Text check modes.
    
    Provided by: TextBasicXnci1Plugin

---
+++ plugin/org/vcssl/nano/plugin/text/xvci1/ContainXvci1PluginTest.java
@@ -0,0 +1,53 @@
+package org.vcssl.nano.plugin.text.xvci1;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.vcssl.connect.ConnectorException;
+import org.vcssl.connect.ExternalVariableConnectorInterface1;
+
+public class ContainXvci1PluginTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		ExternalVariableConnectorInterface1 variable = new ContainXvci1Plugin();
+
+		// Check variable name
+		// 変数名を検査
+		assertEquals("CONTAIN", variable.getVariableName());
+
+		// Check data type
+		// データ型を検査
+		assertTrue(variable.getDataClass() == long.class);
+
+		// Check value
+		// 値を検査
+		try {
+			Long value = (Long)(variable.getData());
+			assertEquals(Long.valueOf(10005L), value);
+		} catch (ConnectorException e) {
+			e.printStackTrace();
+			fail();
+		}
+
+		// Check constantness
+		// 書き換え不可能である事を確認
+		try {
+			variable.setData(Long.valueOf(0L));
+			fail("Expected exception have not occurred");
+		} catch (ConnectorException e) {
+			// 例外が発生するのが正しい挙動
+		}
+	}
+
+}
